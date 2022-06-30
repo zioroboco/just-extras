8cc2594e075746745aab231d09ceee91854d29b3
Louis Quinnell <_@ziorobo.co>
2022-06-30 13:05:22 +1000

    Use typescript for package source

--- tests/blep.test.js
+++ tests/blep.test.js
@@ -1,7 +1,7 @@
 import assert from "node:assert"
 import test from "node:test"
 
-import { blep } from "./blep.mjs"
+import { blep } from "../dist/index.js"
 
 test(`blep has the expected value`, t => {
   assert.equal(blep, "blep")
